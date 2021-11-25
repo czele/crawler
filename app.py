4db8b9bfc6730410a380f08120b8f5958d4b37ef
Camila <camila_zele@hotmail.com>
2021-11-24 22:07:32 -0300

    fiz o cabeçalho

---
+++ app.py
@@ -0,0 +1,15 @@
+import requests
+
+
+header={
+'Host': 'www.jucea.am.gov.br',
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0',
+'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+'Accept-Language': 'pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3',
+'Accept-Encoding': 'gzip, deflate',
+'Referer': 'https://www.google.com/',
+'DNT': '1',
+'Connection': 'keep-alive',
+'Cookie': 'moove_gdpr_popup=%7B%22strict%22%3A%221%22%2C%22thirdparty%22%3A%221%22%2C%22advanced%22%3A%221%22%7D',
+'Upgrade-Insecure-Requests': '1',
+}
